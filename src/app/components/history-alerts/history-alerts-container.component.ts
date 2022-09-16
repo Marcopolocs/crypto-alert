2a2e3e4e7335bd6c45e2b4ebfa9a97107393a34f
Marcopolocs <marcopolocs@gmail.com>
2022-09-16 14:29:31 +0200

    created historyAlert container

---
+++ src/app/components/history-alerts/history-alerts-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-history-alerts-container',
+  templateUrl: './history-alerts-container.component.html',
+  styleUrls: ['./history-alerts-container.component.css']
+})
+export class HistoryAlertsContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
